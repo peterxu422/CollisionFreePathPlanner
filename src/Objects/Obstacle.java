9f556f1b8e25e4c10eed9f00b5ead264a6524518
Peter Xu <peterxu422@gmail.com>
2013-11-18 06:02:25 -0500

    working on Convex Hull

--- src/Objects/Obstacle.java
+++ src/Objects/Obstacle.java
@@ -1,10 +1,14 @@
 package Objects;
 import java.util.ArrayList;
-
+import java.util.Collections;
+import java.util.Stack;
 
 public class Obstacle {
-	ArrayList <Vertex> myPoints;
-	ArrayList <Vertex> myExpandedPoints;
+	private ArrayList <Vertex> myPoints;
+	private ArrayList <Vertex> myExpandedPoints;
+	private ArrayList<Vertex> convexHullPoints;
+	private Vertex p0;
+	private Stack<Vertex> convexStack;
 	private final double robotSize = 0.47;
 	private Robot robotClone;
 	
@@ -12,15 +16,13 @@ public class Obstacle {
 	{
 		this.myPoints = points;
 		robotClone = new Robot(new Vertex(0,0));
-		
 		myExpandedPoints = null;
-		
 	}
 	
 	public Obstacle ()
 	{
 		this.myPoints = new ArrayList <Vertex> ();
-		
+
 		myExpandedPoints = null;
 	}
 	
@@ -40,9 +42,14 @@ public class Obstacle {
 		return this.myExpandedPoints;
 	}
 	
+	public ArrayList<Vertex> getConvexHullPoints()
+	{
+		return this.convexHullPoints;
+	}
 	
 	public void ExpandObstacle()
 	{
+		//TODO Implement
 		myExpandedPoints = new ArrayList<Vertex> (4*myPoints.size());
 		//for(Vertex v : myPoints)
 			//myExpandedPoints.add(v.clone());
@@ -89,6 +96,129 @@ public class Obstacle {
 			
 	}
 	
+	public void computeConvexHull()
+	{
+		//Graham's Algorithm
+		//Testing sample
+		myExpandedPoints = null;
+		myExpandedPoints = new ArrayList<Vertex>(8);
+		myExpandedPoints.add(new Vertex(0,1));
+		myExpandedPoints.add(new Vertex(1,0));
+		myExpandedPoints.add(new Vertex(2,2));
+		myExpandedPoints.add(new Vertex(2,1));
+		myExpandedPoints.add(new Vertex(3,0));
+		myExpandedPoints.add(new Vertex(3,3));
+		myExpandedPoints.add(new Vertex(4,1));
+		myExpandedPoints.add(new Vertex(5,2));
+		//Find Rightmost, Lowest point, P0
+		p0 = getRightLow();
+		assert (p0.getX() == 3.0 && p0.getY() == 0.0);
+		System.out.println("p0:" + p0);
+		
+		System.out.println(myExpandedPoints);
+		//Sort other points angularly about P0. Break ties in favor of closeness. Label sorted P1...PN-1
+		int n = myExpandedPoints.size();
+		assert (n==8);
+		quicksort(0, n-1);
+		
+		System.out.println("After sorting\n"+myExpandedPoints);
+		//Stack stuff
+		convexStack = new Stack<Vertex>();
+		convexStack.push(myExpandedPoints.get(n-1));
+		convexStack.push(myExpandedPoints.get(0));
+		System.out.println("stack init:"+convexStack);
+		int i=1;
+		int j =0;
+		while(i < n)
+		{
+			System.out.println(j++);
+			Vertex ptop = convexStack.pop();
+			Vertex ptop1 = convexStack.peek();
+			Vertex pi = myExpandedPoints.get(i);
+			
+			if(isLeft(pi, ptop, ptop1)) 
+			{
+				convexStack.push(ptop); //push back ptop since it was supposed to be there originally
+				convexStack.push(pi);
+				i++;
+			}
+			//if pi is not strictly left, then pop the top of stack. But this is already handled.
+		}
+		
+		System.out.println("stack: "+convexStack);
+	}
+	
+	public boolean isLeft(Vertex pi, Vertex ptop, Vertex ptop1)
+	{
+		double m = (ptop.getY()-ptop1.getY())/(ptop.getX() - ptop1.getX());
+		double y = ptop.getY();		//Choose a set of points on the line
+		double x = ptop.getX();
+		double b = y - m*x; 		//solve for x-intercept and equation of line 
+		
+		double yi = m*pi.getX() + b;;
+		
+		//slope is 0
+		if(m == 0.0)
+			return false; //False because no x is strictly left of such a line
+		//slope is undefined
+		if(m == Double.POSITIVE_INFINITY || m == Double.NEGATIVE_INFINITY)
+		{
+			if(pi.getX() < x)
+				return true;
+			return false;
+		}
+		if(m < 0) //case when slop is negative
+		{
+			if(pi.getY() < yi)
+				return true;
+			return false;
+		}
+		if(m > 0) //slope is positive
+		{
+			if(pi.getY() > yi)
+				return true;
+			return false;
+		}
+		
+		assert false;
+		return false;
+	}
+	
+	public void quicksort(int low, int high)
+	{
+		int i = low, j = high;
+		Vertex pivot = myExpandedPoints.get(low + (high-low)/2);
+		
+		while(i <= j)
+		{
+			while(myExpandedPoints.get(i).compareWrtP0(pivot, p0) == -1)
+				i++;
+			while(myExpandedPoints.get(j).compareWrtP0(pivot, p0) == 1)
+				j--;
+			
+			if(i <= j) {
+				Collections.swap(myExpandedPoints, i, j);
+				i++;
+				j--;
+			}
+		}
+		
+		if(low < j)
+			quicksort(low, j);
+		if(i < high)
+			quicksort(i, high);
+	}
+	
+	public Vertex getRightLow()
+	{
+		Vertex p0 = myExpandedPoints.get(0);
+		for(Vertex v : myExpandedPoints)
+			if(v.getY() <= p0.getY() && v.getX() >= p0.getX())
+				p0 = v;
+		
+		return p0;
+	}
+	
 	public String toString() {
 		String s = "myPoints: [";
 		for(Vertex v : myPoints)
@@ -103,4 +233,5 @@ public class Obstacle {
 		}
 		return s;
 	}
+
 }
--- src/Objects/Vertex.java
+++ src/Objects/Vertex.java
@@ -65,6 +65,57 @@ public class Vertex {
 		return false;
 	}
 	
+	public int compareDistToP0(Vertex v, Vertex p0)
+	{
+		double p0x = p0.getX();
+		double p0y = p0.getY();
+		double sqx = Math.pow((this.getX() - p0x), 2);
+		double sqy = Math.pow((this.getY() - p0y), 2);
+		double dist = Math.sqrt(sqx + sqy);
+		
+		double vsqx = Math.pow((v.getX() - p0x), 2);
+		double vsqy = Math.pow((v.getY() - p0y), 2);
+		double vdist = Math.sqrt(vsqx + vsqy);
+		
+		if(dist < vdist)
+			return -1;
+		else if(dist > vdist)
+			return 1;
+		
+		return 0;
+	}
+	
+	public int compareAngToP0(Vertex v, Vertex p0)
+	{
+		double angle = Math.toDegrees(Math.atan2(this.getY()-p0.getY(), this.getX()-p0.getX()));
+		double vangle = Math.toDegrees(Math.atan2(v.getY()-p0.getY(), v.getX() - p0.getX()));
+		
+		if(angle < vangle)
+			return -1;
+		else if(angle > vangle)
+			return 1;
+		
+		return 0;
+	}
+	
+	public int compareWrtP0(Vertex v, Vertex p0)
+	{
+		int compAng = this.compareAngToP0(v, p0);
+		if(compAng == -1)
+			return -1;
+		else if(compAng == 1)
+			return 1;
+		
+		int compDist = this.compareDistToP0(v, p0);
+		if(compDist == -1)
+			return -1;
+		else if(compDist == 1)
+			return 1;
+		
+		return 0;
+		
+	}
+	
 	public String toString() {
 		return "("+X+","+Y+")";
 	}
